409732befed419ba24e10d4cdc43f6a034732265
Will Gallia <wgallia@gmail.com>
2015-03-16 10:09:20 +0000

    initial with grabber

---
+++ src/ofApp.cpp
@@ -0,0 +1,23 @@
+#include "ofApp.h"
+
+using namespace ofxCv;
+using namespace cv;
+
+void ofApp::setup() {
+    cam.initGrabber(640, 480);
+    gray.allocate(640, 480, OF_IMAGE_GRAYSCALE);
+}
+
+void ofApp::update() {
+    cam.update();
+    if(cam.isFrameNew()) {
+        convertColor(cam, gray, CV_RGB2GRAY);
+        equalizeHist(gray);
+        gray.update();
+    }
+}
+
+void ofApp::draw() {
+    cam.draw(0, 0);
+    gray.draw(640, 0);
+}
